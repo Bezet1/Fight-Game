0b046190b72affe08184e7e58a873f697448e8b5
Bartek Zientek <joszko90@gmail.com>
2023-02-25 19:06:00 +0100

    added line moving logic

--- app/screens/EnemyRound.js
+++ app/screens/EnemyRound.js
@@ -1,5 +1,5 @@
-import {React, useState, useRef, Component, useEffect} from 'react';
-import { View, Image, Animated, ImageBackground, StyleSheet, SafeAreaView, StatusBar, Text} from 'react-native';
+import {React, useState, useRef, Component, useEffect, useReducer} from 'react';
+import { View, Image, Animated, ImageBackground, StyleSheet, SafeAreaView, StatusBar, Text, Dimensions, Easing} from 'react-native';
 import { useGestureHandlerRef } from 'react-navigation-stack';
 
 import Movable from './MovableHeart';
@@ -8,7 +8,10 @@ function EnemyRound({navigation, route}) {
 
     const oppPath = useRef(route.params.path);
     const [health, setHealth] = useState(0);
+
+    const windowWidth = useRef(Dimensions.get('window'));
     
+    const lineSpeed = useRef(3000);
     const viewX = useRef(0);
     const viewY = useRef(0);
     const viewWidth = useRef(0);
@@ -22,19 +25,21 @@ function EnemyRound({navigation, route}) {
     const line1Y = useRef(0);
     const line1X = useRef(0);
     const [wasHit, setWasHit] = useState(false);
-    const firstUpdate_wasHit = useRef(false);
+    const firstUpdate_lineV = useRef(false);
+    const [startVertical, setStartVertical] = useState(false);
+
+    const randomHeight1 = useRef(0);
+
+    const UpDown1 = useRef();
 
     const ballPosition = useRef(new Animated.ValueXY({x: 20, y: -50})).current;
-    const linePosition1 = useRef(new Animated.ValueXY({x: -100, y: -200})).current;
+    const linePosition1 = useRef(new Animated.ValueXY({x: 0, y: 0})).current;
 
-    function opponent(){
-        return(
-            <View style={styles.opponent}>
-                <Image source={oppPath.current}  style={styles.image100} />
-            </View>
-        )
+    function randomNumber(min, max) { 
+        return Math.random() * (max - min) + min;
     }
 
+    //on layout when start of screen
     function getDimentions(layout){
         viewX.current = layout.x;
         viewY.current = layout.y;
@@ -45,13 +50,27 @@ function EnemyRound({navigation, route}) {
         
         heartX.current = layout.width/2;
         heartY.current = layout.height/2;
-    }
-    
-    function takeHeartPosition(valueX, valueY){
-        heartX.current = valueX + viewWidth_s/2;
-        heartY.current = valueY + viewHeight.current/2;
-        console.log(heartX.current)
+
+        //random height of line on start
+        randomHeight1.current = randomNumber((-viewHeight.current * 0.75)/2, (viewHeight.current * 0.75)/2);
+        linePosition1.setValue({x: -layout.width/2, y: randomHeight1.current})      
+        UpDown1.current = Math.random();
+
+        //start of aniamtions
+        setTimeout(() => {
+            Animated.timing(linePosition1.x, {toValue: viewWidth.current/2 + 20, duration: 6000,easing: Easing.linear ,useNativeDriver: true}).start();
+            setStartVertical((current)=> !current)
+        }, 1000);
+
 }
+    
+    //heart get position
+    function takeHeartPosition(valueX, valueY){
+        heartX.current = valueX + viewWidth.current/2;
+        heartY.current = valueY + viewHeight.current/2;
+    }
+
+    //all listeners
     useEffect(()=> {
         ballPosition.x.addListener(({value}) => {
             ballX.current = value + viewWidth.current/2;
@@ -63,13 +82,26 @@ function EnemyRound({navigation, route}) {
         
         linePosition1.x.addListener(({value}) => {
             line1X.current = value + viewWidth.current/2;
-            if(line1X.current + 30 > heartX.current && line1X.current - 30 < heartX.current && (line1Y.current + 30 < heartY.current || line1Y.current - 30 > heartY.current)){
 
+            if(line1X.current + 30 > heartX.current && line1X.current - 30 < heartX.current && (line1Y.current + 30 < heartY.current || line1Y.current - 30 > heartY.current)){
                 linePosition1.x.removeAllListeners();
                 setHealth((c)=> c + 1)
                 setTimeout(() => {
                     setWasHit((c) => !c)
                 }, 500);
+            }
+
+            //LINE1 end of view
+            if(line1X.current > viewWidth.current + 10){
+                linePosition1.stopAnimation();
+
+                randomHeight1.current = randomNumber((-viewHeight.current * 0.75)/2, (viewHeight.current * 0.75)/2);
+
+                linePosition1.setValue({x: -viewWidth.current/2 - 10, y: randomHeight1.current});
+                UpDown1.current = Math.random();
+
+                setStartVertical((current)=> !current);
+                Animated.timing(linePosition1.x, {toValue: viewWidth.current/2 + 20, duration: 6000,easing: Easing.linear ,useNativeDriver: true}).start();
             }
         })
         
@@ -79,21 +111,61 @@ function EnemyRound({navigation, route}) {
 
     }, [wasHit])
 
+    const duration1  = useRef();
+    const viewForLine1 = useRef();
+    //horizontal line animation
     useEffect(()=> {
-        Animated.loop(
-            Animated.sequence([
-            Animated.timing(linePosition1.y, {toValue: 200, duration: 50000,useNativeDriver: true}),
-                Animated.timing(linePosition1.y, {toValue:-200, duration: 50000, useNativeDriver: true})
-            ])
-        ).start();
+        if(!firstUpdate_lineV.current){
+            firstUpdate_lineV.current = true;
+            return;
+        }
+
+        //LINE1 logic
+        if(!randomHeight1.current){
+            if(UpDown1.current > 0.5){
+                duration1.current = lineSpeed.current;
+                viewForLine1.current = viewHeight.current;
+            }
+            else{
+                duration1.current = lineSpeed.current;
+                viewForLine1.current = -viewHeight.current;
+            }
+        }
+        else{
+            if(UpDown1.current > 0.5){
+                duration1.current = (randomHeight1.current + viewHeight.current/2) * lineSpeed.current/500;
+                viewForLine1.current = viewHeight.current;
+            }
+            else{
+                duration1.current = (-randomHeight1.current + viewHeight.current/2) * lineSpeed.current/500;
+                viewForLine1.current = -viewHeight.current;
+            }
+        }
+
+        console.log('duration: ', duration1.current, '  ','height: ', randomHeight1.current, 'updown: ', UpDown1.current > 0.5)
         
-        Animated.loop(
-            Animated.sequence([
-            Animated.timing(linePosition1.x, {toValue: 100, duration: 18000,useNativeDriver: true}),
-                Animated.timing(linePosition1.x, {toValue:-100, duration: 18000, useNativeDriver: true})
-            ])
-        ).start();
-    }, [])
+        Animated.sequence([
+            Animated.timing(linePosition1.y, {toValue: (-viewForLine1.current * 0.75)/2, duration: duration1.current, easing: Easing.linear, useNativeDriver: true}),
+            Animated.timing(linePosition1.y, {toValue: (viewForLine1.current * 0.75)/2, duration: lineSpeed.current, easing: Easing.linear, useNativeDriver: true})
+            
+        ]).start(({finished})=>{
+            if(finished){
+                randomHeight1.current = 0;
+                setStartVertical((current) => !current);
+            }
+        });
+       
+
+
+    }, [startVertical])
+
+    function opponent(){
+        return(
+            <View style={styles.opponent}>
+                <Image source={oppPath.current}  style={styles.image100} />
+            </View>
+        )
+    }
 
     function ball(){
         return(
@@ -103,7 +175,7 @@ function EnemyRound({navigation, route}) {
 
     function line1(){
         return(
-            <Animated.View style={[styles.lineVertical, {height: viewHeight_s* 2.5, backgroundColor: 'blue',width: viewHeight_s/80 ,transform: [{translateX: linePosition1.x}, {translateY: linePosition1.y}]}]}>
+            <Animated.View style={[styles.lineVertical, {height: viewHeight_s* 2.5,width: viewHeight_s/80 ,transform: [{translateX: linePosition1.x}, {translateY: linePosition1.y}]}]}>
                 <Image resizeMode='contain' style={styles.image100} source={require('../assets/lineHorizontal.png')}></Image>
             </Animated.View>
         )

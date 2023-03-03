faf447e74bb3ee93df9d14eaee5c61fe4464e0d8
Bartek Zientek <joszko90@gmail.com>
2023-03-04 00:32:10 +0100

    changed states

--- app/screens/Homescreen.js
+++ app/screens/Homescreen.js
@@ -13,33 +13,19 @@ import ChooseDifficulty from './ChooseDifficulty';
 
 function Homescreen({navigation}) {
 
-    const[isHomeScreen, setIsHomeScreen] = useState(true);
-    const[isChooseCharacter, setIsChooseCharacter] = useState(false);
-    const[isChooseOpponent, setisChooseOpponent] = useState(false);
-    const[isChooseDifficulty, setIsChooseDifficulty] = useState(false);
+    const[isScreen, setIsScreen] = useState({homeScreen: true, chooseCharacter: false, chooseOpponent: false, chooseDifficulty: false}) 
     const[alertText, setAlertText] = useState([""]);
-    const[is50Pressed, setIs50Pressed] = useState(false);
-    const[is30Pressed, setIs30Pressed] = useState(false);
-    const [myName, setMyName] = useState("");
-    const [oppName, setOppName] = useState("");
-    const [noName, setNoName] = useState(false);
-    const [noCharacter, setNoCharacter] = useState(false);
-    const [noOpponent, setNoOpponent] = useState(false);
-    const [Char1Pressed, setChar1Pressed] = useState(false);
-    const [Char2Pressed, setChar2Pressed] = useState(false);
-    const [Char3Pressed, setChar3Pressed] = useState(false);
-    const [opp1Pressed, setOpp1Pressed] = useState(false);
-    const [opp2Pressed, setOpp2Pressed] = useState(false);
-    const [opp3Pressed, setOpp3Pressed] = useState(false);
-    const [charID, setCharID] = useState('');
-    const [oppID, setOppID] = useState('');
-    const passValues = useRef({health:0, difficulty:""});
+    const[isHpPress, setIsHpPress] = useState({_30: false, _50: false})
+    const [name, setName] = useState({mine: '', opp: ''})
+    const [noElem, setNoElem] = useState({name: false, char: false, opp: false})
+    const [charPress, setCharPress] = useState({_1: false, _2: false, _3: false})
+    const [oppPress, setOppPress] = useState({_1: false, _2: false, _3: false})
+    const passValues = useRef({health:0, difficulty:'', charID: '', oppID: ''});
 
     const aniScale = useRef(new Animated.Value(0.8)).current;
     const aniOpacity = useRef(new Animated.Value(0)).current;
     const damianUP = useRef(new Animated.Value(-2)).current;
     const rudyUP = useRef(new Animated.Value(-5)).current;
-
     const spinValue =  useRef(new Animated.Value(-1)).current
     
     const spinPrzemo = spinValue.interpolate({
@@ -50,27 +36,15 @@ function Homescreen({navigation}) {
     //reset values when focus
     useFocusEffect(
         useCallback(() => { 
-            setIsHomeScreen(()=>false);
-            setIsChooseCharacter(()=>false);
-            setisChooseOpponent(()=>false);
-            setIsChooseDifficulty(()=>true);
+            setIsScreen((obj)=>({...obj, homeScreen: true, chooseCharacter: false, chooseOpponent: false, chooseDifficulty: false}));
             setAlertText(()=>false);
-            setIs50Pressed(()=>false);
-            setIs30Pressed(()=>false);
-            setMyName(()=>"");
-            setOppName(()=>'');
-            setNoName(()=>false);
-            setNoCharacter(()=>false);
-            setNoOpponent(()=>false);
-            setChar1Pressed(()=>false);
-            setChar2Pressed(()=>false);
-            setChar3Pressed(()=>false);
-            setOpp1Pressed(()=>false);
-            setOpp2Pressed(()=>false);
-            setOpp3Pressed(()=>false);
-            setCharID(()=>'');
-            setOppID(()=>'');
-
+            setIsHpPress((obj)=>({...obj, _30: false, _50: false}));
+            setName((obj)=> ({...obj, mine: '', opp: ''}));
+            setNoElem((obj)=>({...obj, name: false, opp: false, char: false}));
+            setCharPress((obj)=> ({...obj, _1: false, _2: false, _3: false}));
+            setOppPress((obj)=> ({...obj, _1: false, _2: false, _3: false}));
+            passValues.current.charID = '';
+            passValues.current.oppID = '';
         }, [])
       );
 
@@ -81,10 +55,10 @@ function Homescreen({navigation}) {
         Animated.spring(aniScale, {toValue: 1, useNativeDriver: true}).start();
         Animated.timing(aniOpacity, {toValue: 1, duration: 200,useNativeDriver: true}).start();
 
-    }, [isHomeScreen, isChooseCharacter, isChooseOpponent, isChooseDifficulty])
+    }, [isScreen.homeScreen, isScreen.chooseCharacter, isScreen.chooseOpponent, isScreen.chooseDifficulty])
 
     useEffect(()=> {
-        if(isChooseOpponent){
+        if(isScreen.chooseOpponent){
             Animated.loop(
                 Animated.sequence([
                     Animated.spring(damianUP, {toValue: 2, useNativeDriver: true, restDisplacementThreshold: 1, restSpeedThreshold: 1, mass: 1}),
@@ -108,33 +82,34 @@ function Homescreen({navigation}) {
             damianUP.stopAnimation();
 
         }
-    }, [isChooseOpponent])
+    }, [isScreen.chooseOpponent])
 
       //homescreen element
       function Home_Screen(){
-        if(isHomeScreen){
+        if(isScreen.homeScreen){
             return(
-                <Home aniOpacity={aniOpacity} setIsHomeScreen={(val)=> setIsHomeScreen(()=> val)} setIsChooseCharacter={(val)=> setIsChooseCharacter(()=> val)} />
+                <Home aniOpacity={aniOpacity} setIsHomeScreen={(val)=> setIsScreen((obj)=> ({...obj, homeScreen: false}))} 
+                setIsChooseCharacter={(val)=> setIsScreen((obj)=> ({...obj, chooseCharacter: true}))} />
             )
         }
       }
 
       function chooseCharacter_Screen(){
-        if(isChooseCharacter){
+        if(isScreen.chooseCharacter){
             return(
-                <ChooseCharacter noCharacter={noCharacter} Char1Pressed={Char1Pressed} firstCharPressed={firstCharPressed}
-                Char2Pressed={Char2Pressed} secondCharPressed={secondCharPressed} Char3Pressed={Char3Pressed} thirdCharPressed={thirdCharPressed}
-                noName={noName} myName={myName} confirmChooseCharacter={confirmChooseCharacter} setNoName={(val) => setNoName(()=>val)}
-                setMyName={(name)=> setMyName(()=>name)} backChooseCharacter={backChooseCharacter} aniScale={aniScale} aniOpacity={aniOpacity}/>
+                <ChooseCharacter noCharacter={noElem.char} Char1Pressed={charPress._1} firstCharPressed={firstCharPressed}
+                Char2Pressed={charPress._2} secondCharPressed={secondCharPressed} Char3Pressed={charPress._3} thirdCharPressed={thirdCharPressed}
+                noName={noElem.name} myName={name.mine} confirmChooseCharacter={confirmChooseCharacter} setNoName={(val) => setNoElem((obj)=> ({...obj, name: val}))}
+                setMyName={(name)=> setName((obj)=> ({...obj, mine: name}))} backChooseCharacter={backChooseCharacter} aniScale={aniScale} aniOpacity={aniOpacity}/>
             )
         }
       }
 
       function chooseOpponent_Screen(){
-        if(isChooseOpponent){
+        if(isScreen.chooseOpponent){
             return(
-                <ChooseOpponent noOpponent={noOpponent} opp1Pressed={opp1Pressed} firstOppPressed={firstOppPressed}
-                opp2Pressed={opp2Pressed} secondOppPressed={secondOppPressed} opp3Pressed={opp3Pressed} thirdOppPressed={thirdOppPressed}
+                <ChooseOpponent noOpponent={noElem.opp} opp1Pressed={oppPress._1} firstOppPressed={firstOppPressed}
+                opp2Pressed={oppPress._2} secondOppPressed={secondOppPressed} opp3Pressed={oppPress._3} thirdOppPressed={thirdOppPressed}
                 confirmChooseOpponent={confirmChooseOpponent} backChooseOpponent={backChooseOpponent} aniScale={aniScale} aniOpacity={aniOpacity}
                 damianUP={damianUP} spinPrzemo={spinPrzemo} rudyUP={rudyUP}/>
             )
@@ -143,150 +118,122 @@ function Homescreen({navigation}) {
 
       //diffuculty element
       function chooseDifficulty_Screen(){
-        if(isChooseDifficulty){
+        if(isScreen.chooseDifficulty){
             return(
-                <ChooseDifficulty backChooseDifficulty={backChooseDifficulty} pressed30={pressed30} is30Pressed={is30Pressed}
-                pressed50={pressed50} is50Pressed={is50Pressed} alertText={alertText} goEasyLevel={goEasyLevel} goHardLevel={goHardLevel}
+                <ChooseDifficulty backChooseDifficulty={backChooseDifficulty} pressed30={pressed30} is30Pressed={isHpPress._30}
+                pressed50={pressed50} is50Pressed={isHpPress._50} alertText={alertText} goEasyLevel={goEasyLevel} goHardLevel={goHardLevel}
                 aniScale={aniScale} aniOpacity={aniOpacity} />
             )
         }
       }
 
       function goEasyLevel(){passValues.current.difficulty = "easy"; startGame();}
+
       function goHardLevel(){passValues.current.difficulty = "hard"; startGame();}
 
       function startGame(){
-        if(!is50Pressed && !is30Pressed){
+        if(!isHpPress._50 && !isHpPress._30){
             setAlertText(()=>["CHOOSE HEALTH!"]);
             return;
         }
         stopAnimation();
 
-        navigation.navigate("Game", {difficulty: passValues.current.difficulty, health: passValues.current.health, char: charID, opp: oppID, myname: myName, oppname: oppName})
+        navigation.navigate("Game", {difficulty: passValues.current.difficulty, health: passValues.current.health, 
+            char: passValues.current.charID, opp: passValues.current.oppID, myname: name.mine, oppname: name.opp})
       }
 
       //when 50 health pressed
       function pressed50(){
-        if(is30Pressed){
-            setIs30Pressed(()=>false);
-        }
-        setIs50Pressed(() => true);
+        setIsHpPress((obj)=>({...obj, _30: false, _50: true}))
         passValues.current.health = 50;
       }
 
       //when 100 health pressed
       function pressed30(){
-        if(is50Pressed){
-            setIs50Pressed(()=>false)
-        }
-        setIs30Pressed(() => true);
+        setIsHpPress((obj)=>({...obj, _30: true, _50: false}))
         passValues.current.health = 30;
       }
 
       //go back pressed
       function backChooseDifficulty(){
-        setIsChooseDifficulty(()=>false)
-        setisChooseOpponent(()=>true);
-        setIs30Pressed(()=> false);
-        setIs50Pressed(()=> false);
+        setIsScreen((obj)=>({...obj, chooseOpponent: true, chooseDifficulty: false}));
+        setIsHpPress((obj)=>({...obj, _30: false, _50: false}))
         setAlertText(()=> []);
       }
 
       function backChooseCharacter(){
-        setIsChooseCharacter(() =>false);
-        setIsHomeScreen(() => true);
-        setChar1Pressed(()=>false);
-        setChar2Pressed(()=> false);
-        setChar3Pressed(()=>false);
-        setNoCharacter(()=> false);
-        setNoName(()=> false)
-        setMyName(()=> "")
+        setIsScreen((obj)=>({...obj, chooseCharacter: false, homeScreen: true}));
+        setCharPress((obj)=> ({...obj, _1: false, _2: false, _3: false}));
+        setNoElem((obj)=> ({...obj, name: false, char: false}))
+        setName((obj)=> ({...obj, mine: ''}))
       }
 
       function backChooseOpponent(){
-        setisChooseOpponent(()=>false);
-        setIsChooseCharacter(()=>true);
-        setOpp1Pressed(()=>false);
-        setOpp2Pressed(()=>false);
-        setOpp3Pressed(()=>false);
-        setNoOpponent(()=>false)
-    }
+        setIsScreen((obj)=>({...obj, chooseOpponent: false, chooseCharacter: true}));
+        setOppPress((obj)=> ({...obj, _1: false, _2: false, _3: false}));
+        setNoElem((obj)=> ({...obj, opp: false}))
+        }
 
       function firstCharPressed(){
-        setChar1Pressed(()=>true);
-        setChar2Pressed(()=> false);
-        setChar3Pressed(()=>false);
-        setNoCharacter(()=> false);
-        setCharID(()=>'1');
+        setCharPress((obj)=> ({...obj, _1: true, _2: false, _3: false}));
+        setNoElem((obj)=> ({...obj, char: false}))
+        passValues.current.charID = '1';
       }
 
       function secondCharPressed(){
-        setChar2Pressed(()=> true);
-        setChar1Pressed(()=>false);
-        setChar3Pressed(()=>false);
-        setNoCharacter(()=> false);
-        setCharID(()=>'2');
+        setCharPress((obj)=> ({...obj, _1: false, _2: true, _3: false}));
+        setNoElem((obj)=> ({...obj, char: false}))
+        passValues.current.charID = '2';
       }
 
       function thirdCharPressed(){
-        setChar3Pressed(()=>true);
-        setChar1Pressed(()=>false);
-        setChar2Pressed(()=> false);
-        setNoCharacter(()=> false);
-        setCharID(()=>'3'); 
+        setCharPress((obj)=> ({...obj, _1: false, _2: false, _3: true}));
+        setNoElem((obj)=> ({...obj, char: false}))
+        passValues.current.charID = '3';}
+        
+    function firstOppPressed(){
+            setOppPress((obj)=> ({...obj, _1: true, _2: false, _3: false}));
+            setNoElem((obj)=> ({...obj, opp: false}))
+            passValues.current.oppID = '1';
+            setName((obj)=> ({...obj, opp: 'PROXIMITY'}))
         }
-        function firstOppPressed(){
-            setOpp1Pressed(()=>true);
-            setOpp2Pressed(()=>false)
-            setOpp3Pressed(()=>false)
-            setNoOpponent(()=>false)
-            setOppID(()=> '1')
-            setOppName(()=>'PROXIMITY')
+    function secondOppPressed(){
+            setOppPress((obj)=> ({...obj, _1: false, _2: true, _3: false}));
+            setNoElem((obj)=> ({...obj, opp: false}))
+            passValues.current.oppID = '2';
+            setName((obj)=> ({...obj, opp: 'PRZEMO'}))
         }
-        function secondOppPressed(){
-            setOpp1Pressed(()=>false);
-            setOpp2Pressed(()=>true)
-            setOpp3Pressed(()=>false)
-            setNoOpponent(()=>false)
-            setOppID(()=> '2')
-            setOppName(()=>'PRZEMO')
-        }
-        function thirdOppPressed(){
-            setOpp1Pressed(()=>false);
-            setOpp2Pressed(()=>false)
-            setOpp3Pressed(()=>true)
-            setNoOpponent(()=>false)
-            setOppID(()=> '3')
-            setOppName(()=>'DJRUDY')
+    function thirdOppPressed(){
+            setOppPress((obj)=> ({...obj, _1: false, _2: false, _3: true}));
+            setNoElem((obj)=> ({...obj, opp: false}))
+            passValues.current.oppID = '3';
+            setName((obj)=> ({...obj, opp: 'DJRUDY'}))
         }
 
       function confirmChooseCharacter(){
-        if(myName == "" && !Char1Pressed && !Char2Pressed && !Char3Pressed){    
-            setNoName(()=> true);
-            setNoCharacter(() => true);
+        if(name.mine == '' && !charPress._1 && !charPress._2 && !charPress._3){    
+            setNoElem((obj)=> ({...obj, name: true, char: true}))
             return;                
         }
-        else if(myName == ""){
-            setNoName(()=> true);
+        else if(name.mine == ''){
+            setNoElem((obj)=> ({...obj, name: true}))
             return;
         }
-        else if(!Char1Pressed && !Char2Pressed && !Char3Pressed){           
-            setNoCharacter(() => true);
+        else if(!charPress._1 && !charPress._2 && !charPress._3){           
+            setNoElem((obj)=> ({...obj, char: true}))
             return;
         }
 
-        setIsChooseCharacter(()=>false);
-        setisChooseOpponent(()=>true);
+        setIsScreen((obj)=>({...obj, chooseCharacter: false, chooseOpponent: true}));
       }
 
       function confirmChooseOpponent(){
-        if(!opp1Pressed && !opp2Pressed && !opp3Pressed){
-            setNoOpponent(()=>true);
+        if(!oppPress._1 && !oppPress._2 && !oppPress._3){
+            setNoElem((obj)=> ({...obj, opp: true}))
             return;
         }
-        setisChooseOpponent(()=> false);
-        setIsChooseDifficulty(()=> true);
 
+        setIsScreen((obj)=>({...obj, chooseOpponent: false, chooseDifficulty: true}));
       }
 
       function stopAnimation(){
